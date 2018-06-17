38e793e7d40375e53baaf7d40ce37b2e73092e67
Chris Leishman <chris@leishman.org>
2018-06-17 10:14:55 -0700

    Deploy kotlin and javadoc to gh-pages

--- site/deploy.sh
+++ site/deploy.sh
@@ -2,22 +2,37 @@
 # script copied from https://github.com/eldarlabs/ghpages-deploy-script, under MIT license.
 
 # abort the script if there is a non-zero error
-set -e
+set -ue
 
-# show where we are on the machine
-pwd
+if [ $# != 1 ]; then
+    echo "Usage: $0 version" 1>&2
+    exit 1;
+fi
 
 version=$1
+
+rootdir=$(cd `dirname $0`/.. && pwd)
+if ! [ -d "$rootdir/.git" ]; then
+    echo "Missing .git directory" 1>&2
+    exit 1
+fi
+
 echo "Publishing for version $version"
 
 remote=$(git config remote.origin.url)
-workingdir="$( dirname "$0" )"
-siteSource="$workingdir/../build/docs/dokka/cava"
-readmeFile="$workingdir/../README.md"
+javadocSource="$rootdir/build/docs/javadoc"
+kotlinSource="$rootdir/build/docs/dokka"
+readmeFile="$rootdir/README.md"
 
-if [ ! -d "$siteSource" ]
+if [ ! -d "$javadocSource" ]
 then
-    echo "$siteSource missing"
+    echo "$javadocSource missing"
+    exit 1
+fi
+
+if [ ! -d "$kotlinSource" ]
+then
+    echo "$kotlinSource missing"
     exit 1
 fi
 
@@ -27,47 +42,57 @@ then
     exit 1
 fi
 
-# make a directory to put the gp-pages branch
-mkdir gh-pages-branch
-cd gh-pages-branch
+# make a temporary directory to put the gp-pages branch
+builddir=$(mktemp -d $TMPDIR/site-XXXXXX)
+trap 'rm -rf $builddir' EXIT
+cd $builddir
+
 # now lets setup a new repo so we can update the gh-pages branch
-git init
-git remote add --fetch origin "$remote"
+echo "Checking out into $builddir/cava"
+git clone --reference "$rootdir" -n "$remote"
+cd cava
 
 # switch into the gh-pages branch
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
 then
     git checkout gh-pages
-    # delete any old site as we are going to replace it
-    # Note: this explodes if there aren't any, so moving it here for now
-    git rm docs/latest
-    if [ -d "docs/${version}" ]
-    then
-        git rm -rf "docs/${version}"
+
+    # cleanup previous pushes
+    if [ -e docs/java/latest ]; then
+        git rm -rf docs/java/latest
+    fi
+    if [ -e "docs/java/${version}" ]; then
+        git rm -rf "docs/java/${version}"
+    fi
+    if [ -e docs/kotlin/latest ]; then
+        git rm -rf docs/kotlin/latest
+    fi
+    if [ -e "docs/kotlin/${version}" ]; then
+        git rm -rf "docs/kotlin/${version}"
     fi
 else
     git checkout --orphan gh-pages
+    git reset --hard
 fi
 
-# copy over or recompile the new site
-mkdir -p docs/$version
-cd docs
-cp "${siteSource}/../style.css" .
-ln -s $version latest
-cd ..
-cp -a "${siteSource}/." "docs/${version}/"
-cp "${readmeFile}" "docs/${version}/"
+# copy documents into place
+mkdir -p docs/java
+(cd docs/java &&
+cp -r "$javadocSource" "./$version" &&
+ln -s "$version" latest)
+
+mkdir -p docs/kotlin
+(cd docs/kotlin &&
+cp -r "$kotlinSource" "./$version" &&
+ln -s "$version" latest)
+
+cp "${readmeFile}" "docs/kotlin/${version}/"
 
 # stage any changes and new files
-git add -A
+git add -A > /dev/null
 # now commit:
 git commit --allow-empty -m "Deploy ${version} to GitHub pages"
 # and push, but send any output to /dev/null to hide anything sensitive
 git push --force --quiet origin gh-pages > /dev/null 2>&1
-
-# go back to where we started and remove the gh-pages git repo we made and used
-# for deployment
-cd ..
-rm -rf gh-pages-branch
 
 echo "Finished Deployment!"
