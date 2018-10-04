9f4e7594fb9211b481ea512f0c74bd6a731fe60f
Chris Leishman <chris@leishman.org>
2018-10-04 09:38:13 +0100

    Use Resources#find for reference test discovery

--- eth-reference-tests/src/test/java/net/consensys/cava/eth/reference/TransactionTestSuite.java
+++ eth-reference-tests/src/test/java/net/consensys/cava/eth/reference/TransactionTestSuite.java
@@ -18,22 +18,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import net.consensys.cava.bytes.Bytes;
 import net.consensys.cava.eth.domain.Address;
 import net.consensys.cava.eth.domain.Transaction;
+import net.consensys.cava.io.Resources;
 import net.consensys.cava.junit.BouncyCastleExtension;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.errorprone.annotations.MustBeClosed;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -44,9 +42,9 @@ class TransactionTestSuite {
 
   private static ObjectMapper mapper = new ObjectMapper();
 
-  @ParameterizedTest(name = "{index}. tx {0}/{1}/{2}")
+  @ParameterizedTest(name = "{index}. tx {0}/{1}")
   @MethodSource("readTransactionTests")
-  void testTransaction(String folder, String name, String milestone, String rlp, String hash, String sender) {
+  void testTransaction(String name, String milestone, String rlp, String hash, String sender) {
     if (hash == null && sender == null) {
       assertThrows(Throwable.class, () -> {
         Transaction tx = Transaction.fromBytes(Bytes.fromHexString(rlp));
@@ -61,36 +59,32 @@ class TransactionTestSuite {
     }
   }
 
+  @MustBeClosed
   private static Stream<Arguments> readTransactionTests() throws IOException {
-    URL testFolder = MerkleTrieTestSuite.class.getClassLoader().getResource("tests");
-    if (testFolder == null) {
-      throw new RuntimeException("Tests folder missing. Please run git submodule --init");
-    }
-    Path folderPath = Paths.get(testFolder.getFile(), "TransactionTests");
-    List<Arguments> tests = Arrays
-        .stream(folderPath.toFile().listFiles(File::isDirectory))
-        .map(folder -> folder.listFiles((file) -> file.getName().endsWith(".json")))
-        .collect(ArrayList::new, (list, files) -> {
-          for (File f : files) {
-            if (!f.getName().contains("GasLimitOverflow")
-                && !f.getName().contains("GasLimitxPriceOverflow")
-                && !f.getName().contains("NotEnoughGas")
-                && !f.getName().contains("NotEnoughGAS")
-                && !f.getName().contains("EmptyTransaction")) {
-              list.addAll(readTestCase(f));
-            }
+    return Resources
+        .find("**/TransactionTests/**/*.json")
+        .filter(
+            url -> !url.getPath().contains("GasLimitOverflow")
+                && !url.getPath().contains("GasLimitxPriceOverflow")
+                && !url.getPath().contains("NotEnoughGas")
+                && !url.getPath().contains("NotEnoughGAS")
+                && !url.getPath().contains("EmptyTransaction"))
+        .flatMap(url -> {
+          try (InputStream in = url.openConnection().getInputStream()) {
+            return readTestCase(in);
+          } catch (IOException e) {
+            throw new UncheckedIOException(e);
           }
-        }, List::addAll);
-    tests.sort(Comparator.comparing(a -> ((String) a.get()[0])));
-    return tests.stream();
+        })
+        .sorted(Comparator.comparing(a -> ((String) a.get()[0])));
   }
 
   @SuppressWarnings({"unchecked", "rawtypes"})
-  private static List<Arguments> readTestCase(File testFile) {
-    try {
-      Map test = mapper.readerFor(Map.class).readValue(testFile);
-      String name = (String) test.keySet().iterator().next();
-      Map testData = (Map) test.get(name);
+  private static Stream<Arguments> readTestCase(InputStream in) throws IOException {
+    Map<String, Map> tests = mapper.readerFor(Map.class).readValue(in);
+    return tests.entrySet().stream().flatMap(entry -> {
+      String name = entry.getKey();
+      Map testData = entry.getValue();
       String rlp = (String) testData.get("rlp");
       List<Arguments> arguments = new ArrayList<>();
       for (String milestone : new String[] {
@@ -102,23 +96,13 @@ class TransactionTestSuite {
           "Homestead"}) {
         Map milestoneData = (Map) testData.get(milestone);
         if (!milestoneData.isEmpty()) {
-          arguments.add(
-              Arguments.of(
-                  testFile.getParentFile().getName(),
-                  name,
-                  milestone,
-                  rlp,
-                  milestoneData.get("hash"),
-                  milestoneData.get("sender")));
+          arguments.add(Arguments.of(name, milestone, rlp, milestoneData.get("hash"), milestoneData.get("sender")));
         }
       }
       if (arguments.isEmpty()) {
-        arguments.add(Arguments.of(testFile.getParentFile().getName(), name, "(no milestone)", rlp, null, null));
+        arguments.add(Arguments.of(name, "(no milestone)", rlp, null, null));
       }
-
-      return arguments;
-    } catch (IOException e) {
-      throw new UncheckedIOException(e);
-    }
+      return arguments.stream();
+    });
   }
 }
