7dcaa07cd32e55148c137cff89b867f662cdce57
Chris Leishman <chris@leishman.org>
2019-01-02 11:43:43 -0800

    An ethereum ÐΞVp2p discovery implementation in Kotlin

--- devp2p/src/main/java/net/consensys/cava/devp2p/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Classes and utilities for Ethereum peer discovery.
- *
- * <p>
- * These classes are included in the standard Cava distribution, or separately when using the gradle dependency
- * 'net.consensys.cava:cava-devp2p' (cava-devp2p.jar).
- */
-package net.consensys.cava.devp2p;
